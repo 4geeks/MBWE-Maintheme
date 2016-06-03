ba78b7b04c937cc978dfa63093d6d86a716ef5b3
Marlon Alfonzo <malfonzo@4geeks.com.ve>
2016-06-03 16:06:42 -0400

    fix why fest and event

--- template-parts/part-why-fetes-events.php
+++ template-parts/part-why-fetes-events.php
@@ -1,4 +1,5 @@
 <div id="why-fetes-events">
+    <div class="custom-overlay"></div>
     <div class="col-md-12"><h2>Why Fetes & Events</h2></div>
     <div class="col-md-4"><center><img width="200" height="200" src="<?php bloginfo('template_url'); ?>/img/WeddingWire-Rated-Black.png"  /></center></div>
     <div class="col-md-8">
