eba6e5f385347eb5b76110403e8605ecc00131a7
Marlon Alfonzo <malfonzo@4geeks.com.ve>
2016-06-10 23:15:15 -0400

    than

---
+++ page-thanks.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Thanks
+*/
+
+get_header(); 
+
+//Get venue post types to list 
+
+?>
+<div class="contact-us">
+	<div class="col-md-12 highlight-p">
+	   <center><h1 class="with-font-title">Responsiveness is one of our main values, <br /> we guaranty you a response in less than 10min</h1></center> 
+<!-- </div> -->
+</div>     
+
+<?php get_footer(); ?>
