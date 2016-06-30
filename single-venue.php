de52d04cc809e6b47d0d4b1422b6945ce1c1b76b
Alejandro Sanchez <a@stcsolutions.com.ve>
2016-06-30 15:03:21 -0400

    venue name and address

--- single-venue.php
+++ single-venue.php
@@ -81,7 +81,7 @@ $venues = new WP_Query( $args );
                 </div>            
                 <div id="div-venue-name">
                     <h1 class="with-font-sub-title" ><?php  echo $name;?></h1> <br />
-                    <span id="direction" > <?php echo $direction ?></span>
+                    <h2 id="direction" > <?php echo $direction ?></h2>
                 </div>
                 <div id="div-venue-button">
                     <a class="btn btn-warning modalContact" href="#animatedModalContact">Request a quote</a>                    
