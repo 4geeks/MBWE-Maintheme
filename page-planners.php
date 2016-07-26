c362d96a467f97c8fb65f9f93ca934be4fd0f76a
Alejandro Sanchez <a@stcsolutions.com.ve>
2016-07-26 17:11:44 -0400

    wedding planners landing page

--- page-planners.php
+++ page-planners.php
@@ -34,13 +34,18 @@ $content = get_page($post->ID)->post_content;
                     $plannerExperience = get_post_meta( $planner->ID, 'wpcf-planner-experience', false)[0];
                     
                     $plannerVideo = get_post_meta( $planner->ID, 'wpcf-planner-video', false)[0];
-                    if(!$plannerVideo or $plannerVideo=='') $plannerVideo = null;
+                    if($plannerVideo and $plannerVideo!='')
+                    {
+                        $videoArray = parse_str( parse_url( $url, PHP_URL_QUERY ), $my_array_of_vars );
+                        $plannerVideo = $videoArray['v'];  
+                    }
+                    else $plannerVideo = null;
                 ?>                        
                     <div class="col-sm-6 col-md-4 planner-element">
                         <div class="div-planner-image" style="background-image: url('<?php echo get_post_meta( $planner->ID, 'wpcf-planner-photo', false)[0]; ?>')">                                
                             <?php if($plannerVideo){ 
                                 if(function_exists('lyte_preparse')) { echo lyte_preparse($plannerVideo); }
-                                else echo '<iframe width="100%" height="100%" src="<?php echo $plannerVideo; ?>?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
+                                else echo '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/<?php echo $plannerVideo; ?>?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
                             } ?>
                         </div>
                         <h3><?php echo get_post_meta( $planner->ID, 'wpcf-planner-full-name', false)[0]; ?></h3>
