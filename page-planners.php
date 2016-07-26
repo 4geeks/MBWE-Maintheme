54d164d958eecc3d8f9eaa953958ad32dde47cbc
Alejandro Sanchez <a@stcsolutions.com.ve>
2016-07-26 15:59:06 -0400

    wedding planners landing page

--- page-planners.php
+++ page-planners.php
@@ -22,8 +22,7 @@ $content = get_page($post->ID)->post_content;
 	<div id="primary" class="full-width-page no-sidebar">
 		<main id="main">
             <?php echo $content; ?>
-            <div id="list-content" class="container">
-                <div class="row">               
+            <div id="list-content" class="container-fluid">
                 <?php foreach ($planners->posts as $planner) {
                     $serviceArea = get_post_meta( $planner->ID, 'wpcf-planner-service-area', false)[0];
                     if(!$serviceArea or $serviceArea=='') $serviceArea = "Miami / Fort Lauderdale area.";
@@ -43,7 +42,6 @@ $content = get_page($post->ID)->post_content;
                         </div>
                     </div>
                <?php }?>
-                </div>
             </div>         
             <div class="blue-background"><h2>What our clients say about our planners</h2></div>   
             <!-- ========== MENU TOP ========== -->
