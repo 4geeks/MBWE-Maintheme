daeb9a5084738c66e64860b94d6fa071a6503054
Alejandro Sanchez <a@stcsolutions.com.ve>
2016-08-04 18:41:14 -0400

    taxonomy page

--- taxonomy.php
+++ taxonomy.php
@@ -10,21 +10,7 @@ get_header();
 $venue_visibility = array('1','2');
 if(isset($_GET["vvisibility"])) $venue_visibility = $_GET["vvisibility"];
 
-$args = array(
-    'post_type' => 'venue',
-    'meta_query' => array(
-        array(
-            'key' => 'wpcf-venue-visibility',
-            'value' => $venue_visibility,
-            'compare' => 'IN'
-        )
-    ),
-    'posts_per_page'=>-1
-    ); 
-$venues = new WP_Query( $args );
-
-get_post();
-
+$category = get_post();
 ?>
     <!-- ========== MENU TOP ========== -->
     <?php get_template_part( 'template-parts/menu', 'top' ); ?>
@@ -61,9 +47,12 @@ get_post();
 				    </div>
 		        </div>
 			</div>
-            <div id="list-content" class="row">                    
-                <?php foreach ($venues->posts as $venue) {
-                    ?>
+            <div id="list-content" class="row">   
+            <?php                  
+	            while ( have_posts() ) {
+	             the_post();
+	             $venue = get_the_post();
+	        ?>
                     <div class="col-sm-12 col-md-6 col-lg-4  venue-element">
                         <div class="col-sm-12 inner-venue-element">
                             <div class="col-sm-12 div-venue-image linkeable highlight-element" style="background-image: url('<?php echo get_post_meta( $venue->ID, 'wpcf-venue-main-image', false)[0]; ?>')">
