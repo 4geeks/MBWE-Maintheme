bbaf3a175c0d5f68701d88da43a34c6fd46f86a9
Alejandro Sanchez <a@stcsolutions.com.ve>
2016-09-16 17:41:13 -0400

    not found website

--- 404.php
+++ 404.php
@@ -1,24 +1,9 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<!-- ========== MENU TOP ========== -->
+    <?php get_template_part( 'template-parts/menu', 'top' ); ?>
+    <!-- ========== MENU TOP ========== -->
+    <div class="blackspace-bar"></div>
+    <?php putRevSlider('not-found'); ?>
 
 <?php get_footer(); ?>
