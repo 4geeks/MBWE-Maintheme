000da5324a9e22458d5075e812e7f781e1f64d89
Alejandro Sanchez <a@stcsolutions.com.ve>
2016-09-16 15:10:48 -0400

    wedding packages landing page

--- template-parts/part-testimonies.php
+++ template-parts/part-testimonies.php
@@ -3,8 +3,7 @@
 $testimonies = types_child_posts('testimonie');
 
 ?>
-
-<?php if(count($testimonies)==0) { ?>                   
+<?php if(count($testimonies)>0) { ?>                   
 <div id="testimonies" class="container">
     <div class="row">
         <div class="col-md-12">
@@ -13,15 +12,19 @@ $testimonies = types_child_posts('testimonie');
     </div>
     <div class="row">
         <?php foreach ($testimonies as $t) { ?>
-            <div class="col-md-6 item-testimonie">
-                    <?php if (isset($t->fields['testimonies-photo']) and $t->fields['testimonies-photo']!=''){ ?>
-                        <div class="img-avatar" style="background-image:url('<?php echo $t->fields['testimonies-photo']; ?>');"></div>
-                    <?php } ?>
-                    <div class="p-testimonie">
-                        <p><?php echo $t->fields['testimonies-testimonie']; ?></p>
-                        <p><?php echo $t->fields['testimonies-author']; ?></p>
-                    </div>
-            </div>                    
+                <?php if(count($testimonies)==1) { ?> 
+                    <div class="col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8  col-lg-offset-3 col-lg-6 item-testimonie">                
+                <?php } else { ?>
+                    <div class="col-md-6 item-testimonie">
+                <?php } ?>
+                        <?php if (isset($t->fields['testimonies-photo']) and $t->fields['testimonies-photo']!=''){ ?>
+                            <div class="img-avatar" style="background-image:url('<?php echo $t->fields['testimonies-photo']; ?>');"></div>
+                        <?php } ?>
+                        <div class="p-testimonie">
+                            <p><?php echo $t->fields['testimonies-testimonie']; ?></p>
+                            <p><?php echo $t->fields['testimonies-author']; ?></p>
+                        </div>
+                </div>                    
         <?php } ?> 
     </div>
 </div>
