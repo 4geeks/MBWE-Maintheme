c28e911d1a9f47031b2458b4ddf8211e23f247f1
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-04-20 12:34:14 -0400

    contact us

--- template-parts/part-contact-us.php
+++ template-parts/part-contact-us.php
@@ -1,12 +1,12 @@
 <div class="contact-us">
-    <div class="col-xs-offset-1 col-xs-6 col-md-5 contact-title visible-sm">
+    <div class="col-xs-offset-1 col-xs-6 col-sm-5 contact-title visible-sm">
            <center>
                 <img class="navbar-logo" src="<?php bloginfo('template_url'); ?>/img/logo_bmw_new.png" />
                 <h3><?php _e( 'Responsiveness is one of our main values, <br /> we guarantee you a response in less than 10min', 'bmw-website' ) ?></h3>
            </center>         
     </div>
     <!-- <div class="row"> -->
-        <div class="col-md-4 col-xs-12 contact-call-us center-text">
+        <div class="col-sm-4 col-xs-12 contact-call-us center-text">
             <center class='visible-md visible-lg'>
                 <img class="navbar-logo" src="<?php bloginfo('template_url'); ?>/img/logo_bmw_new.png" />
             </center> 
