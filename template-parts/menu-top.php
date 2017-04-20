0d4ec45dca75a47b4af557c6b829b7a521756f0e
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-04-20 13:03:29 -0400

    wedding churches

--- template-parts/menu-top.php
+++ template-parts/menu-top.php
@@ -277,7 +277,7 @@ $otherIdeas = get_terms( 'idea-type', array(
                     </li>
                 </ul>               
             </li>
-        <?php if (is_page('gallery') || is_page('gallery-event') || is_singular("venue")){ ?>
+        <?php if (is_page('gallery') || is_page('gallery-event') || is_singular("venue") || is_singular("church")){ ?>
             <li id="secondary-menu">
                 <?php get_template_part( 'template-parts/part', 'top-venue-nomobile' ); ?>
             </li>
